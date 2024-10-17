83872e5478c74cca9278dccf43ec5b5ea231c6c8
NaCl <vitus.tseng@gmail.com>
2024-10-18 01:12:09 +0800

    finished Scrapy

--- hw2/hw2/hw2_mlb_stats/get_data/get_data/settings.py
+++ hw2/hw2/hw2_mlb_stats/get_data/get_data/settings.py
@@ -91,3 +91,8 @@ ROBOTSTXT_OBEY = True
 REQUEST_FINGERPRINTER_IMPLEMENTATION = "2.7"
 TWISTED_REACTOR = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
 FEED_EXPORT_ENCODING = "utf-8"
+
+DEPTH_PRIORITY = 1
+CONCURRENT_REQUESTS = 1
+SCHEDULER_DISK_QUEUE = 'scrapy.squeues.PickleFifoDiskQueue'
+SCHEDULER_MEMORY_QUEUE = 'scrapy.squeues.FifoMemoryQueue'
--- hw2/hw2/hw2_mlb_stats/get_data/get_data/spiders/get_data_mlb.py
+++ hw2/hw2/hw2_mlb_stats/get_data/get_data/spiders/get_data_mlb.py
@@ -1,31 +1,40 @@
 import scrapy
-
+from scrapy.selector import Selector
 
 class GetDataMlbSpider(scrapy.Spider):
     name = "get_data_mlb"
     allowed_domains = ["www.mlb.com"]
-    start_urls = ["https://www.mlb.com/stats/"]
+
+    def start_requests(self):
+        for page in range(1, 5):
+            yield scrapy.Request(f"https://www.mlb.com/stats/?page={page}")
 
     def parse(self, response):
-        for player in response.xpath("//tr[@class='p1']"):
-            name = player.xpath("td[@class='dg-name_display_first_last']/a/text()").get()
-            position = player.xpath("td[@class='dg-position']/text()").get()
-            team = player.xpath("td[@class='dg-team_abbrev']/text()").get()
-            at_bats = player.xpath("td[@class='dg-ab']/text()").get()
-            runs = player.xpath("td[@class='dg-r']/text()").get()
-            hits = player.xpath("td[@class='dg-h']/text()").get()
-            doubles = player.xpath("td[@class='dg-2b']/text()").get()
+        sel = Selector(response)
+        players_list = sel.css(".notranslate > tr")
 
-            # Print extracted data for debugging
-            print(f"Name: {name}, Position: {position}, Team: {team}, At Bats: {at_bats}, Runs: {runs}, Hits: {hits}, Doubles: {doubles}")
-
-            if name and position and team and at_bats and runs and hits and doubles:
-                yield {
-                    'name': name,
-                    'position': position,
-                    'team': team,
-                    'at_bats': at_bats,
-                    'runs': runs,
-                    'hits': hits,
-                    'doubles': doubles,
-                }
+        for player in players_list:
+            first_name =  player.css("th:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > a:nth-child(1) > span:nth-child(1)::text").extract_first()
+            second_name = player.css("th:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > a:nth-child(1) > span:nth-child(3)::text").extract_first()
+            player_data = {
+                "PLAYER": first_name + " " + second_name,
+                "TEAM": player.css("td:nth-child(2)::text").extract_first(),
+                "G": player.css("td:nth-child(3)::text").extract_first(),
+                "AB": player.css("td:nth-child(4)::text").extract_first(),
+                "R": player.css("td:nth-child(5)::text").extract_first(),
+                "H": player.css("td:nth-child(6) > a:nth-child(1)::text").extract_first(),
+                "2B": player.css("td:nth-child(7) > a:nth-child(1)::text").extract_first(),
+                "3B": player.css("td:nth-child(8)::text").extract_first(),
+                "HR": player.css("td:nth-child(9) > a:nth-child(1)::text").extract_first(),
+                "RBI": player.css("td:nth-child(10)::text").extract_first(),
+                "BB": player.css("td:nth-child(11) > a:nth-child(1)::text").extract_first(),
+                "SO": player.css("td:nth-child(12) > a:nth-child(1)::text").extract_first(),
+                "SB": player.css("td:nth-child(13)::text").extract_first(),
+                "CS": player.css("td:nth-child(14)::text").extract_first(),
+                "AVG": player.css("td:nth-child(15)::text").extract_first(),
+                "OBP": player.css("td:nth-child(16)::text").extract_first(),
+                "SLG": player.css("td:nth-child(17)::text").extract_first(),
+                "OPS": player.css("td:nth-child(18)::text").extract_first()
+            }
+            player_data = {k: (v if v is not None else '0') for k, v in player_data.items()}
+            yield player_data
